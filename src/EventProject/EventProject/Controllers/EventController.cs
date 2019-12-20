6cd46c4f5e481f68e7ad3e04c1614b91a8687865
Nguyen Huu Quyen <huuquyen1412@gmail.com>
2019-12-20 18:01:32 +0700

    upadate model
    misssing shortdec cause error render

--- src/EventProject/EventProject/Controllers/EventController.cs
+++ src/EventProject/EventProject/Controllers/EventController.cs
@@ -23,6 +23,7 @@ namespace EventProject.Controllers
             var model = from x in eventLs
                         orderby x.EventStartDate descending
                         select x;
+
             return View(model);
         }
     }
